93abb08f1e9e7c42da62aefc3a45c24ce14c420a
root <root@ip-172-31-33-192.us-west-2.compute.internal>
2018-03-15 16:17:46 +0000

    added get *

--- aws-back/index.js
+++ aws-back/index.js
@@ -2,14 +2,22 @@ const express = require('express'),
           app = express(),
          PORT = process.env.PORT || 8080
 
+app.use(express.static(__dirname+'/build'));
+
+
 app.use(function(req,res,next){
   res.header('Access-Control-Allow-Origin', '*')
   next()
 })
 
-app.get('/', (req,res)=>{
+app.get('/test', (req,res)=>{
   res.send(`We're sending stuff out on AWS!`)
 })
+
+app.get('*', (req,res)=>{
+  res.sendFile(__dirname+'/build/index.html')
+})
+
 
 app.listen(PORT, ()=>{
   console.log(`We are listening on ${PORT}.`)
